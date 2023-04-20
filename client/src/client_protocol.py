46437ce5a6e9ce439d452991853173d250e85484
andrezszambrano <andreskilla2000@gmail.com>
2023-04-20 14:16:07 -0300

    Protocol methods created to send weather and stations data

--- client/src/client_protocol.py
+++ client/src/client_protocol.py
@@ -1,10 +1,54 @@
+from .client import MONTREAL, TORONTO, WASHINGTON
 from .protocol import Protocol
 from .packet import Packet
+
 
 class ClientProtocol(Protocol):
 
     def __init__(self):
         super(ClientProtocol, self).__init__()
+        self._city_name_to_char = {MONTREAL: 'M', WASHINGTON: 'W', TORONTO: 'T'}
 
-    def send(self):
-        pass
+    def send_weather_data(self, socket, city_name, weather_list):
+        packet = Packet()
+        for weather in weather_list:
+            packet.add_byte(self._city_name_to_char[city_name])
+            packet.add_date(weather.date)
+            self._add_float_else_none(weather.prectot)
+            self._add_float_else_none(weather.qv2m)
+            self._add_float_else_none(weather.rh2m)
+            self._add_float_else_none(weather.ps)
+            self._add_float_else_none(weather.t2m_range)
+            self._add_float_else_none(weather.ts)
+            self._add_float_else_none(weather.t2mdew)
+            self._add_float_else_none(weather.t2mwet)
+            self._add_float_else_none(weather.t2m_max)
+            self._add_float_else_none(weather.t2m_min)
+            self._add_float_else_none(weather.t2m)
+            self._add_float_else_none(weather.ws50m_range)
+            self._add_float_else_none(weather.ws10m_range)
+            self._add_float_else_none(weather.ws50m_min)
+            self._add_float_else_none(weather.ws10m_min)
+            self._add_float_else_none(weather.ws50m_max)
+            self._add_float_else_none(weather.ws10m_max)
+            self._add_float_else_none(weather.ws50m)
+            self._add_float_else_none(weather.ws10m)
+        packet.send_to_socket(socket)
+
+    def __add_float_else_none(self, packet, float):
+        if float is None:
+            packet.add_byte(self.NO_FLOAT)
+        else:
+            packet.add_byte(self.FLOAT)
+            packet.add_float(float)
+
+    def send_station_data(self, socket, city_name, stations_list):
+        packet = Packet()
+        for station in stations_list:
+            packet.add_byte(self._city_name_to_char[city_name])
+            packet.add_n_byte_number(self.TWO_BYTES, station.code)
+            packet.add_string_and_length(station.name)
+            packet.__add_float_else_none(station.latitude)
+            packet.__add_float_else_none(station.longitude)
+            packet.add_n_byte_number(self.TWO_BYTES, station.yearid)
+        packet.send_to_socket(socket)
--- client/src/sender.py
+++ client/src/sender.py
@@ -1,5 +1,6 @@
 import logging
 
+from .client_protocol import ClientProtocol
 from .socket_wrapper import Socket
 
 WEATHER_DATA = "W"
@@ -10,17 +11,20 @@ class Sender:
     def __init__(self, server_address):
         host, _port = server_address.split(':')
         port = int(_port)
-        #self._socket = Socket(host, port)
+        self._socket = Socket(host, port)
 
     def send_data(self, queue):
         counter = 0
         keep_receiving = True
+        protocol = ClientProtocol()
         while keep_receiving:
             data = queue.get()
             if data[0] == FINISHED:
                 counter = counter + 1
                 keep_receiving = not (counter == 3)
             elif data[0] == WEATHER_DATA:
-                logging.debug(f"{data[1]}{data[2][0].date} and {data[2][1].date}")
+                protocol.send_weather_data(self._socket, data[1], data[2])
+                #logging.debug(f"{data[1]}{data[2][0].date} and {data[2][1].date}")
             else:
-                logging.debug(f"{data[1]}{data[2][0].name} and {data[2][1].name}")
+                protocol.send_station_data(self._socket, data[1], data[2])
+                #logging.debug(f"{data[1]}{data[2][0].name} and {data[2][1].name}")
