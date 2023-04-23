7cd0dab17dfe6adf3ec5f02328e2ce6c3f86d92c
andrezszambrano <andreskilla2000@gmail.com>
2023-04-23 20:43:53 -0300

    CommunicationHandler class created

---
+++ client/src/communication_handler.py
@@ -0,0 +1,29 @@
+from .client_protocol import ClientProtocol
+from .packet import Packet
+
+
+class CommunicationHandler:
+    def __init__(self, socket):
+        self._socket = socket
+
+    def send_station_data(self, city_name, stations_list):
+        packet = Packet()
+        protocol = ClientProtocol()
+        protocol.add_stations_chunk_to_packet(packet, city_name, stations_list)
+        protocol.add_request_for_ack_to_packet(packet)
+        self._socket.send(packet)
+        self._socket.recv(1)
+
+    def send_weather_data(self, city_name, weathers_list):
+        packet = Packet()
+        protocol = ClientProtocol()
+        protocol.add_weather_chunk_to_packet(packet, city_name, weathers_list)
+        protocol.add_request_for_ack_to_packet(packet)
+        self._socket.send(packet)
+        self._socket.recv(1)
+
+    def send_finished(self):
+        protocol = ClientProtocol()
+        packet = Packet()
+        protocol.add_finished_to_packet(packet)
+        self._socket.send(packet)
--- client/src/sender.py
+++ client/src/sender.py
@@ -1,6 +1,7 @@
 import logging
 
 from .client_protocol import ClientProtocol
+from .communication_handler import CommunicationHandler
 from .socket_wrapper import Socket
 from .packet import Packet
 
@@ -17,36 +18,15 @@ class Sender:
     def send_data(self, queue):
         counter = 0
         keep_receiving = True
+        communication_handler = CommunicationHandler(self._socket)
         while keep_receiving:
             data = queue.get()
             if data[0] == FINISHED:
                 counter = counter + 1
                 keep_receiving = not (counter == 3)
             elif data[0] == WEATHER_DATA:
-                self.__handle_weather_data(data[1], data[2])
+                communication_handler.send_weather_data(data[1], data[2])
             else:
-                self.__handle_station_data(data[1], data[2])
-        self.__send_finished_packet()
+                communication_handler.send_station_data(data[1], data[2])
+        communication_handler.send_finished()
         self._socket.shutdown_and_close()
-
-    def __handle_station_data(self, city_name, stations_list):
-        packet = Packet()
-        protocol = ClientProtocol()
-        protocol.add_stations_chunk_to_packet(packet, city_name, stations_list)
-        protocol.add_request_for_ack_to_packet(packet)
-        self._socket.send(packet)
-        self._socket.recv(1)
-
-    def __handle_weather_data(self, city_name, weather_list):
-        packet = Packet()
-        protocol = ClientProtocol()
-        protocol.add_weather_chunk_to_packet(packet, city_name, weather_list)
-        protocol.add_request_for_ack_to_packet(packet)
-        self._socket.send(packet)
-        self._socket.recv(1)
-
-    def __send_finished_packet(self):
-        protocol = ClientProtocol()
-        packet = Packet()
-        protocol.add_finished_to_packet(packet)
-        self._socket.send(packet)
