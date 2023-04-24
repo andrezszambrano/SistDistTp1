5595ab908afd24fd82b39680fde9b3d2334083ab
andrezszambrano <andreskilla2000@gmail.com>
2023-04-24 00:18:49 -0300

    Refactor in client communication handler

--- client/src/client_communication_handler.py
+++ client/src/client_communication_handler.py
@@ -12,7 +12,7 @@ class ClientCommunicationHandler:
         protocol.add_stations_chunk_to_packet(packet, city_name, stations_list)
         protocol.add_request_for_ack_to_packet(packet)
         self._socket.send(packet)
-        self._socket.read(1)
+        protocol.recv_ack(self._socket)
 
     def send_weather_data(self, city_name, weathers_list):
         packet = Packet()
@@ -20,7 +20,7 @@ class ClientCommunicationHandler:
         protocol.add_weather_chunk_to_packet(packet, city_name, weathers_list)
         protocol.add_request_for_ack_to_packet(packet)
         self._socket.send(packet)
-        self._socket.read(1)
+        protocol.recv_ack(self._socket)
 
     def send_finished(self):
         protocol = ClientProtocol()
--- client/src/client_protocol.py
+++ client/src/client_protocol.py
@@ -16,3 +16,6 @@ class ClientProtocol(Protocol):
 
     def add_request_for_ack_to_packet(self, packet):
         packet.add_byte(super().ASK_ACK)
+
+    def recv_ack(self, byte_stream):
+        super()._recv_byte(byte_stream)
