c9caa2cc6e156612d04f4b71f6aaa9116a001101
andrezszambrano <andreskilla2000@gmail.com>
2023-04-26 17:06:21 -0300

    Running average is printed for the days that rained

--- server/src/processes/weather_processor.py
+++ server/src/processes/weather_processor.py
@@ -9,14 +9,15 @@ class WeatherProcessor:
 
     def __init__(self, data_queue, trips_queue_n_id_tuple):
         self._data_queue = data_queue
-        self._days_that_rained_in_city = set()
+        self._days_that_rained_in_city = {}
         self._trips_queue = trips_queue_n_id_tuple[0]
         self._trips_queue_id = trips_queue_n_id_tuple[1]
 
     def run(self):
         self._recv_weather_data()
         self._recv_trips_data()
-
+        for city_n_date in self._days_that_rained_in_city:
+            logging.debug(f"{city_n_date}: {self._days_that_rained_in_city[city_n_date].get_avg()}")
 
     def _recv_weather_data(self):
         weather_communication_handler = QueueCommunicationHandler(self._data_queue)
@@ -25,7 +26,7 @@ class WeatherProcessor:
             if weather_data is None:
                 break
             elif weather_data.prectot > self.MIN_PRECTOT:
-                self._days_that_rained_in_city.update({(weather_data.city_name, weather_data.date), RunningAverage()})
+                self._days_that_rained_in_city.update({(weather_data.city_name, weather_data.date): RunningAverage()})
         #logging.debug(f"Days that rained:{self._days_that_rained_in_city}")
 
     def _recv_trips_data(self):
@@ -39,7 +40,4 @@ class WeatherProcessor:
     def _process_trip(self, trip_data):
         if (trip_data.city_name, trip_data.start_date_time.date) in self._days_that_rained_in_city:
             self._days_that_rained_in_city[(trip_data.city_name, trip_data.start_date_time.date)].recalculate_avg(trip_data.duration)
-            logging.debug(f"It rained")
-        else:
-            logging.debug(f"Not")
         #logging.debug(f"{trip_data.info()}")
--- server/src/utils/running_average.py
+++ server/src/utils/running_average.py
@@ -9,3 +9,8 @@ class RunningAverage:
         self._total = self._total + number
         self._count = self._count + 1
         return self._total / self._count
+
+    def get_avg(self):
+        if self._total == 0:
+            return 0
+        return self._total / self._count
