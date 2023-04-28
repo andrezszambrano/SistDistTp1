c1e43b849b061f48231e0507a67ffa2968098e84
andrezszambrano <andreskilla2000@gmail.com>
2023-04-28 20:12:39 -0300

    Logic changed in weather processor, results still the same

--- server/src/counter.py
+++ server/src/counter.py
@@ -2,11 +2,12 @@ import logging
 
 
 class Counter:
-    def __init__(self):
+    def __init__(self, name):
         self._counter = 0
+        self._name = name
 
     def increase(self):
         self._counter = self._counter + 1
         #logging.debug(f"{self._counter}")
-        if self._counter % 1000000 == 0:
-            logging.debug(f"{self._counter}")
+        if self._counter % 100 == 0:
+            logging.debug(f"{self._name}: {self._counter}")
--- server/src/processes/weather_processor.py
+++ server/src/processes/weather_processor.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import logging
 
 from ..communication_handlers.queue_communication_handler import QueueCommunicationHandler
+from ..counter import Counter
 from ..utils.running_average import RunningAverage
 
 
@@ -10,17 +11,18 @@ class WeatherProcessor:
 
     def __init__(self, data_queue, trips_queue_n_id_tuple):
         self._data_queue = data_queue
-        self._days_that_rained_in_city = {}
+        self._days_that_rained_in_city = set()
+        self._date_to_avg_dict = {}
         self._trips_queue = trips_queue_n_id_tuple[0]
         self._trips_queue_id = trips_queue_n_id_tuple[1]
 
     def run(self):
         self._recv_weather_data()
         self._recv_trips_data()
-        final_dict = self._obtain_date_n_average_dict()
-        for date in final_dict:
-            if final_dict[date].get_avg() > 0:
-                logging.debug(f"{date}: {final_dict[date].get_avg()}")
+        #final_dict = self._obtain_date_n_average_dict()
+        for date in self._date_to_avg_dict:
+            if self._date_to_avg_dict[date].get_avg() > 0:
+                logging.debug(f"{date}: {self._date_to_avg_dict[date].get_avg()}")
 
     def _recv_weather_data(self):
         weather_communication_handler = QueueCommunicationHandler(self._data_queue)
@@ -29,23 +31,29 @@ class WeatherProcessor:
             if weather_data is None:
                 break
             elif weather_data.prectot > self.MIN_PRECTOT:
-                self._days_that_rained_in_city.update({(weather_data.city_name, weather_data.date): RunningAverage()})
+                self._days_that_rained_in_city.add((weather_data.city_name, weather_data.date))
         #logging.debug(f"Days that rained:{self._days_that_rained_in_city}")
 
     def _recv_trips_data(self):
         trip_communication_handler = QueueCommunicationHandler(self._trips_queue, self._trips_queue_id)
+        counter = Counter("WeatherProcess")
         while True:
             trip_data = trip_communication_handler.recv_trip_data()
             if trip_data is None:
                 break
             self._process_trip(trip_data)
+            counter.increase()
 
     def _process_trip(self, trip_data):
-        key = (trip_data.city_name, trip_data.start_date_time.date())
         #logging.debug(f"{key}")
-        if key in self._days_that_rained_in_city:
+        date =  trip_data.start_date_time.date()
+        if (trip_data.city_name, date) in self._days_that_rained_in_city:
+            if date in self._date_to_avg_dict:
+                self._date_to_avg_dict[date].recalculate_avg(trip_data.duration_sec)
+            else:
+                self._date_to_avg_dict.update({date: RunningAverage(trip_data.duration_sec, 1)})
             #logging.debug(f"{trip_data.info()}")
-            self._days_that_rained_in_city[key].recalculate_avg(trip_data.duration_sec)
+            #self._days_that_rained_in_city[key].recalculate_avg(trip_data.duration_sec)
 
     def _obtain_date_n_average_dict(self):
         date_n_average = {}
