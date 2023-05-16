a68f2fca5655e400e09658027ff27a6bfe15bb98
andrezszambrano <andreskilla2000@gmail.com>
2023-05-15 22:07:33 -0300

    ClientApi is closed gracefully

--- server/client_main_api_processor/src/client_main_api_processor.py
+++ server/client_main_api_processor/src/client_main_api_processor.py
@@ -1,4 +1,5 @@
 import logging
+import signal
 
 from .acceptor_socket import AcceptorSocket
 from .queue_communication_handler import QueueCommunicationHandler
@@ -15,16 +16,27 @@ class ClientMainApiProcessor:
         self._acceptor_socket = AcceptorSocket('', port, listen_backlog)
         self._client_processes = []
         self._channel = channel
-        self._queue_for_all_data = RabbProdConsQueue(channel, "AllData")
+        self._finished_bool = MutableBoolean(False)
+        queue_for_all_data = RabbProdConsQueue(channel, "AllData")
+        self._all_data_sender_communication_handler = QueueCommunicationHandler(queue_for_all_data)
+        socket = self._acceptor_socket.accept()
+        self._client_communicator_handler = SocketCommunicationHandler(socket)
+        signal.signal(signal.SIGTERM, self.__exit_gracefully)
+
+    def __exit_gracefully(self, _signum, _frame):
+        logging.info("Exiting gracefully")
+        self.__close_resources()
 
     def run(self):
-        socket = self._acceptor_socket.accept()
-        client_communicator_handler = SocketCommunicationHandler(socket)
-        distributor_communicator_handler = QueueCommunicationHandler(queue=self._queue_for_all_data)
-        finished_bool = MutableBoolean(False)
-        while not finished_bool.get_boolean():
-            action = client_communicator_handler.recv_action()
-            action.perform_action(finished_bool, client_communicator_handler, distributor_communicator_handler)
-        socket.shutdown_and_close()
-        self._acceptor_socket.shutdown_and_close()
+        while not self._finished_bool.get_boolean():
+            action = self._client_communicator_handler.recv_action()
+            action.perform_action(self._finished_bool, self._client_communicator_handler,
+                                  self._all_data_sender_communication_handler)
+        self.__close_resources()
         logging.debug(f"finished")
+
+    def __close_resources(self):
+        self._client_communicator_handler.close()
+        self._all_data_sender_communication_handler.close()
+        self._acceptor_socket.shutdown_and_close()
+        self._channel.close()
