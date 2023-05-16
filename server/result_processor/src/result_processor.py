2e1360afe393f5830e1c36b19d9b58387fcf9f3c
andrezszambrano <andreskilla2000@gmail.com>
2023-05-15 21:39:41 -0300

    Results exits gracefully

--- server/result_processor/src/result_processor.py
+++ server/result_processor/src/result_processor.py
@@ -1,4 +1,5 @@
 import logging
+import signal
 
 from .mutable_boolean import MutableBoolean
 from .packet import Packet
@@ -12,24 +13,33 @@ from .counter import Counter
 class ResultMonitorProcessor:
     def __init__(self, channel):
         self._channel = channel
-        self._results_monitor_queue = RabbProdConsQueue(channel, "ResultData", self.__process_result_data)
-        self._result_monitor_communication_handler = QueueCommunicationHandler(None)
+        results_queue = RabbProdConsQueue(channel, "ResultData", self.__process_result_data)
+        self._results_recv_communication_handler = QueueCommunicationHandler(results_queue)
+        self._communication_handler = QueueCommunicationHandler(None)
         self._printing_counter = PrintingCounter("Registered in result", 10000)
         self._counter = Counter()
         query_queue = RabbProdConsQueue(channel, "QueryData")
         self._query_communication_handler = QueueCommunicationHandler(query_queue)
         self._query_results = QueryData({}, {2016: {}, 2017: {}}, {}, False)
+        self._finished_boolean = MutableBoolean(False)
+        signal.signal(signal.SIGTERM, self.__exit_gracefully)
+
+    def __exit_gracefully(self, _signum, _frame):
+        logging.info("Exiting gracefully")
+        self._results_recv_communication_handler.close()
+        self._query_communication_handler.close()
+        self._channel.stop_consuming()
+        self._channel.close()
 
     def __process_result_data(self, _ch, _method, _properties, body):
-        action = self._result_monitor_communication_handler.recv_results_processor_action(Packet(body))
+        action = self._communication_handler.recv_results_processor_action(Packet(body))
         action.perform_action__(self._finished_boolean, self._counter, self._query_results, self._query_communication_handler,
                                 self._printing_counter)
         if self._finished_boolean.get_boolean():
             self._channel.stop_consuming()
 
     def run(self):
-        self._finished_boolean = MutableBoolean(False)
-        self._results_monitor_queue.start_recv_loop()
+        self._results_recv_communication_handler.start_consuming()
         self._channel.close()
-        logging.info(f"Finished receiving weather data")
+        logging.info(f"Finished receiving results data")
 
